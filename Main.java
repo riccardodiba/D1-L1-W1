d44971959e555ebe4a0bb93336c23b9a6eb77d52
Riccardo Di Bari <riccardodibari@hotmail.it>
2023-11-27 19:05:53 +0100

    prova due

---
+++ Main.java
@@ -0,0 +1,15 @@
+public class Main {
+        public static void main(String[] args) {
+
+            System.out.println(moltiplica(10,5));
+            System.out.println(concatena("riccardo",5));
+        }
+
+        public static int moltiplica(int num1, int num2){
+            return num1 * num2;
+        }
+
+        public static String concatena(String stringa, int num){
+            return stringa + num;
+        }
+    }
